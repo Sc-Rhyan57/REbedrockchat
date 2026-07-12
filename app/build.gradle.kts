78f98250366df8f19d4a7eb7a23864307b3b708a
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-07-12 02:05:19 -0300

    Upgrade MinecraftAuth dependency to version 5.0.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -76,7 +76,7 @@ dependencies {
         exclude(group = "org.cloudburstmc.math", module = "api")
     }
 
-    implementation("net.raphimc:MinecraftAuth:4.0.2") {
+    implementation("net.raphimc:MinecraftAuth:5.0.0") {
         exclude(group = "org.slf4j")
     }
 
