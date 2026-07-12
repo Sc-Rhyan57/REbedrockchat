c1756e28fc62ebff86b1f3df11623d3cdb8a924e
Sc-Rhyan57 <rhyanlus501@gmail.com>
2026-07-12 00:46:26 -0300

    {PUBLIC V} TEST V2

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -10,6 +10,9 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven("https://repo.opencollab.dev/main/") { name = "opencollab" }
+        maven("https://repo.opencollab.dev/maven-snapshots/") { name = "opencollab-snapshots" }
+        maven("https://maven.lenni0451.net/releases") { name = "lenni0451" }
     }
 }
 rootProject.name = "SevenVoice"
